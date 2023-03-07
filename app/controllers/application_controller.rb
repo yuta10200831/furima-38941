195bc3e23e8634134a0b51c8444ef3b63b464c8a
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-03-07 19:29:06 +0900

    sign_inの記述消去

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,5 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :email, :password, :family_name, :first_name, :family_name_kana, :first_name_kana, :birth_day])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:email, :password])
   end
 end
