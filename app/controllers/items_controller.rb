a379bebffe117a63d78792e38319f3be20abcd17
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-03-12 15:53:51 +0900

    商品情報編集の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user_id == current_user.id
+    else
+      redirect_to root_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.save
+      redirect_to item_path
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def item_params
