c90316349e03a42578a3984436e1d066bbd26c7f
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-03-12 19:52:17 +0900

    商品削除機能の修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -40,8 +40,11 @@ class ItemsController < ApplicationController
 
   def destroy
     item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
   end
   private
 
