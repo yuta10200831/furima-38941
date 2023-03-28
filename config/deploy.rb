c3fe65def0b3e230b68854e4a2250963d8025ceb
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-03-28 21:54:32 +0900

    aws環境設定⑦

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.17.2'
 set :application, 'furima-38941'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:yuta10200831/furma-38941.git'
+set :repo_url, 'git@github.com:yuta10200831/furima-38941.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
